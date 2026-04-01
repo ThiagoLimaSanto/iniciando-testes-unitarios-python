dfa8aa463258334ee18381aea192cc1b3aab5df5
Thiago Lima <tlima7601@gmail.com>
2026-04-01 14:40:59 -0300

    Projeto criado

---
+++ testes_unitarios/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for testes_unitarios project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testes_unitarios.settings')
+
+application = get_asgi_application()
